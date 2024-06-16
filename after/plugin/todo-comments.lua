a4c467a2fe4fa90c1bb546543c4deb7b469eca22
Daniel Heery <danieljheery@gmail.com>
2024-06-16 09:35:38 +0100

    Add @example comment type to todo-comments plugin'

--- after/plugin/todo-comments.lua
+++ after/plugin/todo-comments.lua
@@ -8,7 +8,8 @@ tdc.setup({
      QUESTION = { icon = "", color = "#FFBF00" },
      WARNING = { icon = "", color = "#ED2939" },
      ["MY_SPEC"] = { icon = "", color = "#f6fd22", alt = {"!!"} },
-     ["TDL"] = { icon = " ", color = "#BA992f" }
+     ["TDL"] = { icon = " ", color = "#BA992f", alt = { "ff", "_TODO_" } },
+     example = { icon = "", color="#95D5B2" }
  },
  highlight = {
   multiline = false,
@@ -20,12 +21,19 @@ tdc.setup({
 -- HACK: This is hacky
 -- @param: myParam This is my param 
 -- @returns: this method returns 
--- PERF: Performance 
--- NOTE: This is a note 
+-- @PERF: Performance 
+-- @NOTE: This is a note 
 -- !WARNING: This is a warining
 -- !FIX:  Fix Me Please;
--- QUESTION: This is a question
--- >TODO: Multi line disables
+-- ?QUESTION: This is a question
+-- TODO: Multi line disables
 -- @TODO: A Now todo
 -- @param: 
 -- >TDL: my todo 
+-- <TDL>: My TODO
+-- [TODO]: 
+-- [[: 
+-- <___>: 
+-- !TODO!: 
+-- _TODO_: 
+-- @example: hello 
