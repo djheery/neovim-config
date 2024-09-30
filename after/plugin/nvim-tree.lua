1efcb33570112f084a2c4700068fdc6b192d7f44
Daniel Heery <danieljheery@gmail.com>
2024-09-30 10:34:10 +0100

    Focus Nvim-tree keybind

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,3 +4,4 @@ vim.g.loaded_netrwPlugin = 1;
 require("nvim-tree").setup(); 
 
 vim.keymap.set('n', '<leader>pv', ':NvimTreeFindFileToggle<CR>');
+vim.keymap.set('n', '<leader>pr', ':NvimTreeFocus<CR>'); 
